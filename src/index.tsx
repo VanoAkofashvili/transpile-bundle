eea77522151a9e4fd1719beb05d21726c0b22386
Vaniko Akopashvili <v.akopashvili@sweeftdigital.com>
2021-11-19 22:01:27 +0400

    Seperate logic into components

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,12 @@ import ReactDOM from 'react-dom';
 import { fetchPlugin } from './plugins/fetch-plugin';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import CodeEditor from './components/code-editor';
+import Preview from './components/preview';
 
 const App = () => {
   const ref = useRef<any>();
-  const iframe = useRef<any>();
   const [input, setInput] = useState('');
-
+  const [code, setCode] = useState('');
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
@@ -33,8 +33,6 @@ const App = () => {
     //   target: 'es2015',
     // });
 
-    iframe.current.srcdoc = html;
-
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -46,30 +44,8 @@ const App = () => {
       },
     });
 
-    // setCode(result.outputFiles[0].text);
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+    setCode(result.outputFiles[0].text);
   };
-
-  const html = `
-    <html>
-      <head>
-      </head>
-      <body>
-        <div id="root"></div>
-        <script>
-          window.addEventListener('message', (event) => {
-            try {
-              eval(event.data);
-            } catch(err) {
-              const root = document.querySelector('#root');
-              root.innerHTML = '<div style="color:red;"><h4>Runtime Error</h4>' + err + '</div>'
-              console.err(err);
-            }
-          }, false);
-        </script>
-      </body>
-    </html>
-  `;
 
   return (
     <div>
@@ -77,19 +53,10 @@ const App = () => {
         initialValue="const a = 'vano'"
         onChange={(value) => setInput(value)}
       />
-      <textarea
-        onChange={(e) => setInput(e.target.value)}
-        value={input}
-      ></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <iframe
-        ref={iframe}
-        srcDoc={html}
-        sandbox="allow-scripts"
-        title="preview"
-      />
+      <Preview code={code} />
     </div>
   );
 };
